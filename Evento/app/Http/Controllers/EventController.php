9a313ad62aab1523ed8e86674e54809432419f27
Mouhcine Essafi <messafi1337@gmail.com>
2024-03-07 20:11:47 +0100

    add controller event with all methods for gestion

---
+++ Evento/app/Http/Controllers/EventController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function showForm()
+    {
+        $content = file_get_contents('https://raw.githubusercontent.com/alaouy/sql-moroccan-cities/master/json/ville.json');
+        $data = json_decode($content);
+        $categories = Category::all();
+        return view('organisateur.formEvent', compact('data', 'categories'));
+    }
+
+    public function AllEvents()
+    {
+        $events = Event::all();
+        return view('organisateur.evento', compact('events'));
+    }
+
+    public function ShowEvent()
+    {
+        $events = Event::all();
+        return view('organiser.allEvents', compact('events'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $user = Auth::id();
+
+        $request->validate([
+            'title' => 'required',
+            'location' => 'required',
+            'price' => 'required',
+            'date' => 'required',
+            'time' => 'required',
+            'description' => 'required',
+            'reservation_type' => 'required',
+            'image' => 'required|image',
+            'category_id' => 'required',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $fileName = time() . $request->file('image')->getClientOriginalName();
+            $path = $request->file('image')->storeAs('image', $fileName, 'public');
+            $picturePath = Storage::url($path);
+        } else {
+            $picturePath = null;
+        }
+
+        Event::create([
+            'title' => $request->title,
+            'location' => $request->location,
+            'date' => $request->date,
+            'time' => $request->time,
+            'price' => $request->price,
+            'nbr_place' => $request->nbr_place,
+            'description' => $request->description,
+            'reservation_type' => $request->reservation_type,
+            'image' => $picturePath,
+            'creator' => $user,
+            'category_id' => $request->category_id,
+        ]);
+
+        return redirect('/evento');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function editEvent($id)
+    {
+        $content = file_get_contents('https://raw.githubusercontent.com/alaouy/sql-moroccan-cities/master/json/ville.json');
+        $data = json_decode($content);
+
+        $event = Event::find($id);
+
+        return view('organiser.updateEvent', compact('event', 'data'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function updateEvent(Request $request, $id)
+    {
+        $user = Auth::id();
+        $event = Event::findOrFail($id);
+
+        $request->validate([
+            'title' => 'required',
+            'location' => 'required',
+            'price' => 'required',
+            'date' => 'required',
+            'time' => 'required',
+            'description' => 'required',
+            'reservation_type' => 'required',
+            'image' => 'required',
+        ]);
+
+
+        $event->title = $request['title'];
+        $event->location = $request['location'];
+        $event->date = $request['date'];
+        $event->time = $request['time'];
+        $event->price = $request['price'];
+        $event->nbr_place = $request['nbr_place'];
+        $event->description = $request['description'];
+        $event->reservation_type = $request['reservation_type'];
+        $event->image = $request['image'];
+        $event->creator = $user;
+
+        $event->save();
+        return redirect('/allEvents');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function deleteEvent(string $id)
+    {
+        $event = Event::findOrFail($id);
+        $event->delete();
+
+        return redirect()->back();
+    }
+}
